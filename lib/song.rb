6b9b146831e9506c0fce6e742b5cfc35972c0caa
Natalie Taktachev <ntaktach@gmail.com>
2019-07-26 23:12:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
     @@all.sort_by{|song| song.name}
   end
 
-  def new_from_filename(filename)
+  def self.new_from_filename(filename)
     filename.split(" - ")
     artist_name = filename[0]
     song_name = filename[1].gsub(".mp3","")
